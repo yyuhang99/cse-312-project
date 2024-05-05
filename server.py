a63a73141a7a6127b8149c928b82b2edb9436d07
bchow3 <bchow3466@gmail.com>
2024-05-05 14:08:28 -0400

    added block limiter for 30 sec

--- server.py
+++ server.py
@@ -16,8 +16,11 @@ from collections import defaultdict
 def rate_limiter():
     ip = request.remote_addr
     current_time = int(time.time())
+    if (block_times[ip] - current_time < 30):
+        return jsonify({"error": "Too Many Requests"}), 429
     request_counters[ip] = [t for t in request_counters[ip] if current_time - t < 10]
     if len(request_counters[ip]) >= 50:
+        block_times[ip].append(current_time)
         return jsonify({"error": "Too Many Requests"}), 429
     request_counters[ip].append(current_time)
     
@@ -30,6 +33,7 @@ app.before_request(rate_limiter)
 socket = SocketIO(app)
 socket.init_app(app, cors_allowed_origins="*")
 request_counters = defaultdict(list)
+block_times = defaultdict(int)
 
 statistics = {
     "posts_created": 0,
